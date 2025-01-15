25c209119ac4bbaf26add1169c754588a75da85e
Alex Meyer <ameyer@cs.utexas.edu>
2025-01-14 21:05:54 -0600

    Generalize big-endian integer types with macros

--- crates/device-tree/src/be.rs
+++ crates/device-tree/src/be.rs
@@ -1,52 +1,37 @@
 #![allow(non_camel_case_types)]
 
-#[derive(Copy, Clone, PartialEq, Eq)]
-#[repr(transparent)]
-pub struct u32_be {
-    inner: u32,
-}
-#[must_use]
-pub const fn u32_be(val: u32) -> u32_be {
-    u32_be {
-        inner: u32::to_be(val),
-    }
-}
-impl u32_be {
-    #[must_use]
-    pub fn get(self) -> u32 {
-        u32::from_be(self.inner)
-    }
-}
-unsafe impl bytemuck::Pod for u32_be {}
-unsafe impl bytemuck::Zeroable for u32_be {}
+macro_rules! define_endian_int {
+    ($name:ident, $base:ty, $from:ident, $to:ident) => {
+        #[derive(Copy, Clone, PartialEq, Eq)]
+        #[repr(transparent)]
+        pub struct $name {
+            inner: $base,
+        }
 
-#[derive(Copy, Clone, PartialEq, Eq)]
-#[repr(transparent)]
-pub struct u64_be {
-    inner: u64,
-}
-#[must_use]
-pub const fn u64_be(val: u64) -> u64_be {
-    u64_be {
-        inner: u64::to_be(val),
-    }
-}
-impl u64_be {
-    #[must_use]
-    pub fn get(self) -> u64 {
-        u64::from_be(self.inner)
-    }
-}
-unsafe impl bytemuck::Pod for u64_be {}
-unsafe impl bytemuck::Zeroable for u64_be {}
+        #[must_use]
+        pub const fn $name(val: $base) -> $name {
+            $name {
+                inner: <$base>::$to(val),
+            }
+        }
 
-impl core::fmt::Debug for u32_be {
-    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-        core::fmt::Debug::fmt(&self.get(), f)
-    }
+        impl $name {
+            #[must_use]
+            pub fn get(self) -> $base {
+                <$base>::$from(self.inner)
+            }
+        }
+
+        unsafe impl bytemuck::Pod for $name {}
+        unsafe impl bytemuck::Zeroable for $name {}
+
+        impl core::fmt::Debug for $name {
+            fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+                core::fmt::Debug::fmt(&self.get(), f)
+            }
+        }
+    };
 }
-impl core::fmt::Debug for u64_be {
-    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-        core::fmt::Debug::fmt(&self.get(), f)
-    }
-}
+
+define_endian_int!(u32_be, u32, from_be, to_be);
+define_endian_int!(u64_be, u64, from_be, to_be);
