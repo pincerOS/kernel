e87f64e0bb7bb3f410d2afac26bc09aefc56482e
Slava A <46628638+Slava-A1@users.noreply.github.com>
2025-03-07 14:55:22 -0600

    User processes can map a given physical address to a given virtual address (#39)
    * now able to map a physical page to a given virtual address in the kernel
    
    * changed function return types to use result
    
    * adding requested changes
    
    * fixed copying of data from hello string to mapped page
    
    * complete pa to va mapping for user with some temp hacks for testing
    
    * pa to va for user now takes in ttrb0 instead of the translation table struct
    
    * clean up of va to pa user code
    
    * changed initialization of intermediate page allocator
    
    * removing some unecessary public access
    
    * very minor formatting change
    
    * adjustments to test case
    
    * more test case fixes
    
    * implemented requested changes

---
+++ crates/kernel/examples/vaToPaUser.rs
@@ -0,0 +1,124 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+extern crate kernel;
+
+use crate::event::context;
+use crate::event::thread;
+use alloc::boxed::Box;
+use core::arch::asm;
+use kernel::*;
+
+//Current version of create_user_table leaves this addr free
+const VIRTUAL_ADDR: usize = 0x1E00000;
+static HELLO_CHARS: [u8; 5] = *b"hello";
+static WORLD_CHARS: [u8; 5] = *b"world";
+
+#[repr(C, align(4096))]
+struct SomePage([u8; 4096]);
+
+#[no_mangle]
+extern "Rust" fn kernel_main(_device_tree: device_tree::DeviceTree) {
+    println!("| starting kernel_main");
+
+    unsafe { crate::arch::memory::init_physical_alloc() };
+
+    // Create user region (mapped at 0x20_0000 in virtual memory)
+    let (user_region, ttbr0) = unsafe { crate::arch::memory::create_user_region() };
+
+    // Mark current thread as using TTBR0, so that preemption saves
+    // and restores the register.
+    context::CORES.with_current(|core| {
+        let mut thread = core.thread.take().unwrap();
+        thread.user_regs = Some(thread::UserRegs {
+            sp_el0: 0,
+            ttbr0_el1: ttbr0,
+            usermode: false,
+        });
+        core.thread.set(Some(thread));
+    });
+    // Enable the user-mode address space in this thread
+    unsafe {
+        asm!("msr TTBR0_EL1, {0}", "isb", "dsb sy", "tlbi vmalle1is", "dsb sy", in(reg) ttbr0)
+    };
+
+    println!("User ptr: {:p}", user_region);
+    // TODO: sometimes get an insn abort here? (leads to UART deadlock)
+    println!(
+        "Physical addr: {:?}",
+        memory::physical_addr(user_region.addr())
+    );
+    let access = crate::arch::memory::at_s1e0r(user_region.addr());
+    println!(
+        "user access: {:?}",
+        access.map(|b| b.bits()).map_err(|e| e.bits())
+    );
+
+    //hack: get a "page" from the heap and try to map it to some virtual address
+    let page_box = Box::new(SomePage([0; 4096]));
+    let page_ptr: *mut SomePage = Box::into_raw(page_box);
+    println!("page_ptr: {:x}", page_ptr as usize);
+    //Copying hello into the first few bytes of the page
+    let phys_addr: usize = crate::arch::memory::physical_addr((page_ptr).addr()).unwrap() as usize;
+
+    unsafe {
+        core::ptr::copy_nonoverlapping(
+            &raw const HELLO_CHARS[0],
+            page_ptr as *mut u8,
+            HELLO_CHARS.len(),
+        );
+    }
+
+    println!(
+        "Attempting to map pa {:x} to va: {:x}",
+        phys_addr, VIRTUAL_ADDR
+    );
+    unsafe {
+        match crate::arch::memory::map_pa_to_va_user(phys_addr, VIRTUAL_ADDR, ttbr0) {
+            Ok(()) => println!("Done mapping!"),
+            Err(e) => println!("Error: {}", e),
+        }
+    }
+
+    let virt_ptr: *const u8 = VIRTUAL_ADDR as *const u8;
+    let mut all_good = true;
+
+    for i in 0..5 {
+        if unsafe { *(virt_ptr.wrapping_add(i)) != HELLO_CHARS[i] } {
+            all_good = false;
+            break;
+        }
+    }
+
+    if all_good {
+        println!("Passed first check");
+    } else {
+        println!("First check failed");
+    }
+
+    all_good = true;
+
+    unsafe {
+        core::ptr::copy_nonoverlapping(
+            &raw const WORLD_CHARS[0],
+            VIRTUAL_ADDR as *mut u8,
+            WORLD_CHARS.len(),
+        );
+    }
+
+    for i in 0..5 {
+        if unsafe { (*page_ptr).0[i] != WORLD_CHARS[i] } {
+            all_good = false;
+            break;
+        }
+    }
+
+    if all_good {
+        println!("Passed second check");
+    } else {
+        println!("Second check failed");
+    }
+
+    println!("Done with basic pa to va user mapping test!");
+}
