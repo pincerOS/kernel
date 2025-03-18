2b19e83c77528a364456fff61a72cc8bcb24a1b3
Bobby Youstra <bobbyyoustra@protonmail.com>
2025-03-18 10:31:21 +0100

    Read-only Ext2 (#34)
    * Basic filesystem boilerplate
    
    * add some structs
    
    * read superblock
    
    * add inode size function
    
    * added some inode code
    
    * added initial read-only FS code
    
    * fixing some superblock/inode bugs
    
    * got basic file reading working!
    
    * finished first fs reading test
    
    * fixed truncation issue
    
    * ran cargo fmt
    
    * making all module members public
    
    * made block size not a constant and used Result type now
    
    * backported statically sized name_characters array
    
    * renamed logical block name
    
    * Fix warnings
    
    * Remove length parameter from read_* methods
    
    * Lazily parse directory entries
    
    * added read logical multiple block functions
    
    * ran cargo fmt
    
    * Fix reading multi-block BGDT
    
    ---------
    
    Co-authored-by: Alex Meyer <ameyer@cs.utexas.edu>
    Co-authored-by: Sasha Huang <huang@cs.utexas.edu>

---
+++ crates/filesystem/src/tests.rs
@@ -0,0 +1,32 @@
+#[cfg(feature = "std")]
+extern crate std;
+
+use crate::{linux::FileBlockDevice, Ext2, INodeWrapper};
+use alloc::rc::Rc;
+use std::fs::File;
+
+#[test]
+fn example_1() {
+    let file = File::open("example_1.img").unwrap();
+    let disk = FileBlockDevice::new(file);
+
+    let mut ext2 = Ext2::new(disk).unwrap();
+
+    assert_eq!(ext2.get_block_size(), 1024);
+    assert_eq!(ext2.get_inode_size(), 128);
+
+    let root_node: Rc<INodeWrapper> = ext2.get_root_inode_wrapper();
+
+    let test_node: Rc<INodeWrapper> = ext2.find(&root_node, b"test.txt").unwrap();
+    let test_node_text = test_node.read_text_file_as_str(&mut ext2).unwrap();
+
+    assert_eq!(test_node_text, "asldfalsjdkfvnlasdfvnka,dsfvmna");
+
+    let test_folder: Rc<INodeWrapper> = ext2.find(&root_node, b"folder").unwrap();
+    let test_file_in_folder = ext2.find(&test_folder, b"asdf.txt").unwrap();
+    let test_file_in_folder_text = test_file_in_folder
+        .read_text_file_as_str(&mut ext2)
+        .unwrap();
+
+    assert_eq!(test_file_in_folder_text, "Hi");
+}
