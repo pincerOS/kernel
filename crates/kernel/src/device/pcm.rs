7cbb691ff3f3186989d2bcbaf597cdfb3865c344
Sasha Huang <huang@cs.utexas.edu>
2025-03-23 15:39:54 -0500

    pcm stuff part 4/?

--- crates/kernel/src/device/pcm.rs
+++ crates/kernel/src/device/pcm.rs
@@ -35,6 +35,9 @@ const PCM_FRAME_SYNC_SLAVE: usize = 1;
 const PCM_FRAME_SYNC_HIGH: usize = 0; // default
 const PCM_FRAME_SYNC_LOW: usize = 1; // inverted
 
+const CHANNEL_RXC: usize = 0x00;
+const CHANNEL_TXC: usize = 0x04;
+
 pub struct bcm2711_pcm_driver {
     base_addr: *mut (), /// 0x7e203000
 }
@@ -116,15 +119,15 @@ impl bcm2711_pcm_driver {
         )
     }
 
-    fn check_bit(&mut self, reg: usize, bit: usize) -> bool {
-        unsafe { self.reg_cs.read() & (1 << bit) != 0 }
+    fn check_bit(&mut self, reg: Volatile<u32>, bit: usize) -> bool {
+        unsafe { reg.read() & (1 << bit) != 0 }
     }
     
-    fn read_bits(&mut self, reg: usize, start: usize, len: usize) -> u32 {
-        unsafe { (self.reg_cs.read() & (((1 << len) - 1) << start)) >> start }
+    fn read_bits(&mut self, reg: Volatile<u32>, start: usize, len: usize) -> u32 {
+        unsafe { (reg.read() & (((1 << len) - 1) << start)) >> start }
     }
 
-    fn set_bit(&mut self, reg: usize, bit: usize, on: bool) -> bool {
+    fn set_bit(&mut self, reg: Volatile<u32>, bit: usize, on: bool) -> bool {
         if self.check_bit(reg, bit) == on {
             false
         } else {
@@ -133,36 +136,36 @@ impl bcm2711_pcm_driver {
         }
     }
 
-    fn set_bit_force(&mut self, reg: usize, bit: usize, on: bool) {
-        unsafe { self.reg_cs.write(if on {self.reg_cs.read() | (1 << bit)} else {self.reg_cs.read() & !(1 << bit)}) };
+    fn set_bit_force(&mut self, reg: Volatile<u32>, bit: usize, on: bool) {
+        unsafe { reg.write(if on {reg.read() | (1 << bit)} else {reg.read() & !(1 << bit)}) };
     }
 
     fn write_bits(&mut self, reg: usize, start: usize, len: usize, bits: u32) {
-        unsafe { self.reg_cs.write(self.reg_cs.read() | ((bits & ((1 << len) - 1)) << start)) }
+        unsafe { reg.write(reg.read() | ((bits & ((1 << len) - 1)) << start)) }
     }
 
     fn pcm_is_enabled(&mut self) -> bool {
-        self.check_bit(CS_A, 0)
+        self.check_bit(self.reg_cs(), 0)
     }
 
     fn toggle_pcm(&mut self, on: bool) -> bool {
-        self.set_bit(CS_A, 0, on)
+        self.set_bit(self.reg_cs(), 0, on)
     }
 
     fn reception_is_enabled(&mut self) -> bool {
-        self.check_bit(CS_A, 1)
+        self.check_bit(self.reg_cs(), 1)
     }
 
     fn toggle_reception(&mut self, on: bool) -> bool {
-        self.set_bit(CS_A, 1, on)
+        self.set_bit(self.reg_cs(), 1, on)
     }
 
     fn transmission_is_enabled(&mut self) -> bool {
-        self.check_bit(CS_A, 2)
+        self.check_bit(self.reg_cs(), 2)
     }
 
     fn toggle_transmission(&mut self, on: bool) -> bool {
-        self.set_bit(CS_A, 2, on)
+        self.set_bit(self.reg_cs(), 2, on)
     }
 
     fn reset_transmission_fifo(&mut self) -> bool {
@@ -170,7 +173,7 @@ impl bcm2711_pcm_driver {
             false
         }
         else {
-            self.set_bit(CS_A, 3, true)
+            self.set_bit(self.reg_cs(), 3, true)
         }
     }
 
@@ -179,12 +182,12 @@ impl bcm2711_pcm_driver {
             false
         }
         else {
-            self.set_bit(CS_A, 4, true)
+            self.set_bit(self.reg_cs(), 4, true)
         }
     }
 
     fn get_transmission_fifo_threshold(&mut self) {
-        self.read_bits(CS_A, 5, 2)
+        self.read_bits(self.reg_cs(), 5, 2)
     }
 
     fn set_transmission_fifo_threshold(&mut self, val: u32) -> bool {
@@ -192,13 +195,13 @@ impl bcm2711_pcm_driver {
             false
         }
         else {
-            self.write_bits(CS_A, 5, 2, val);
+            self.write_bits(self.reg_cs(), 5, 2, val);
             true
         }
     }
 
     fn get_reception_fifo_threshold(&mut self) {
-        self.read_bits(CS_A, 7, 2)
+        self.read_bits(self.reg_cs(), 7, 2)
     }
 
     fn set_reception_fifo_threshold(&mut self, val: u32) -> bool {
@@ -206,13 +209,13 @@ impl bcm2711_pcm_driver {
             false
         }
         else {
-            self.write_bits(CS_A, 7, 2, val);
+            self.write_bits(self.reg_cs(), 7, 2, val);
             true
         }
     }
 
     fn dma_dreq_is_enabled(&mut self) -> bool {
-        self.check_bit(CS_A, 9)
+        self.check_bit(self.reg_cs(), 9)
     }
 
     fn toggle_dma_dreq(&mut self, on: bool) -> bool {
@@ -220,24 +223,24 @@ impl bcm2711_pcm_driver {
             false
         }
         else {
-            self.set_bit(CS_A, 9, on)
+            self.set_bit(self.reg_cs(), 9, on)
         }
     }
 
     fn transmission_fifo_in_sync(&mut self) -> bool {
-        self.check_bit(CS_A, 13)
+        self.check_bit(self.reg_cs(), 13)
     }
 
     fn reception_fifo_in_sync(&mut self) -> bool {
-        self.check_bit(CS_A, 14)
+        self.check_bit(self.reg_cs(), 14)
     }
 
     fn transmission_fifo_errored(&mut self) -> bool {
-        self.check_bit(CS_A, 15)
+        self.check_bit(self.reg_cs(), 15)
     }
 
     fn reception_fifo_errored(&mut self) -> bool {
-        self.check_bit(CS_A, 16)
+        self.check_bit(self.reg_cs(), 16)
     }
 
     fn clear_transmission_fifo_error(&mut self) -> bool {
@@ -245,7 +248,7 @@ impl bcm2711_pcm_driver {
             false
         }
         else {
-            self.set_bit(CS_A, 15, true)
+            self.set_bit(self.reg_cs(), 15, true)
         }
     }
 
@@ -254,36 +257,36 @@ impl bcm2711_pcm_driver {
             false
         }
         else {
-            self.set_bit(CS_A, 16, true)
+            self.set_bit(self.reg_cs(), 16, true)
         }
     }
 
     fn transmission_fifo_needs_writing(&mut self) -> bool {
-        self.check_bit(CS_A, 17)
+        self.check_bit(self.reg_cs(), 17)
     }
 
     fn reception_fifo_needs_reading(&mut self) -> bool {
-        self.check_bit(CS_A, 18)
+        self.check_bit(self.reg_cs(), 18)
     }
     
     fn transmission_fifo_available(&mut self) -> bool {
-        self.check_bit(CS_A, 19)
+        self.check_bit(self.reg_cs(), 19)
     }
 
     fn reception_fifo_available(&mut self) -> bool {
-        self.check_bit(CS_A, 20)
+        self.check_bit(self.reg_cs(), 20)
     }
 
     fn transmission_fifo_empty(&mut self) -> bool {
-        self.check_bit(CS_A, 21)
+        self.check_bit(self.reg_cs(), 21)
     }
     
     fn reception_fifo_full(&mut self) -> bool {
-        self.check_bit(CS_A, 22)
+        self.check_bit(self.reg_cs(), 22)
     }
 
     fn reception_sex_enabled(&mut self) -> bool {
-        self.check_bit(CS_A, 23)
+        self.check_bit(self.reg_cs(), 23)
     }
 
     fn toggle_reception_sex(&mut self) -> bool {
@@ -291,34 +294,34 @@ impl bcm2711_pcm_driver {
             false
         }
         else {
-            self.set_bit(CS_A, 23, true)
+            self.set_bit(self.reg_cs(), 23, true)
         }
     }
 
     // for checking the PCM clock cycle.
     // value written here will be read back 2 cycles later
     fn check_clock_sync(&mut self) -> bool {
-        self.check_bit(CS_A, 24)
+        self.check_bit(self.reg_cs(), 24)
     }
     fn toggle_clock_sync(&mut self, on: bool) -> bool {
         if self.pcm_is_enabled() {
             false
         }
         else {
-            self.set_bit(CS_A, 24, on)
+            self.set_bit(self.reg_cs(), 24, on)
         }
     }
 
     fn fifo_read(&mut self) -> u32 {
-        self.read_bits(FIFO_A, 0, 32)
+        self.read_bits(self.reg_fifo(), 0, 32)
     }
 
     fn fifo_write(&mut self, data: u32) {
-        self.write_bits(FIFO_A, 0, 32, data)
+        self.write_bits(self.reg_fifo(), 0, 32, data)
     }
 
     fn pcm_clock_disabled(&mut self) -> bool {
-        self.check_bit(MODE_A, 28)
+        self.check_bit(self.reg_mode(), 28)
     }
 
     fn toggle_pcm_clock(&mut self, disabled: bool) -> bool {
@@ -326,12 +329,12 @@ impl bcm2711_pcm_driver {
             false
         }
         else {
-            self.set_bit(MODE_A, 28, disabled)
+            self.set_bit(self.reg_mode(), 28, disabled)
         }
     }
 
     fn get_pdm_decimation_factor(&mut self) -> u32 {
-        if self.check_bit(MODE_A, 27) {32} else {16}
+        if self.check_bit(self.reg_mode(), 27) {32} else {16}
     }
 
     fn set_pdm_decimation_factor(&mut self, factor: u32) -> bool {
@@ -340,10 +343,10 @@ impl bcm2711_pcm_driver {
         }
         else {
             if factor == 16 {
-                self.set_bit(MODE_A, 27, false)
+                self.set_bit(self.reg_mode(), 27, false)
             }
             else if factor == 32 {
-                self.set_bit(MODE_A, 27, true)
+                self.set_bit(self.reg_mode(), 27, true)
             }
             else {
                 false
@@ -352,7 +355,7 @@ impl bcm2711_pcm_driver {
     }
 
     fn pdm_enabled(&mut self) -> bool {
-        self.check_bit(MODE_A, 26)
+        self.check_bit(self.reg_mode(), 26)
     }
 
     fn toggle_pdm(&mut self, on: bool) -> bool {
@@ -360,16 +363,16 @@ impl bcm2711_pcm_driver {
             false
         }
         else {
-            self.set_bit(MODE_A, 26, on)
+            self.set_bit(self.reg_mode(), 26, on)
         }
     }
 
     fn reception_frame_packing_enabled(&mut self) -> bool {
-        self.check_bit(MODE_A, 25)
+        self.check_bit(self.reg_mode(), 25)
     }
 
     fn transmission_frame_packing_enabled(&mut self) -> bool {
-        self.check_bit(MODE_A, 24)
+        self.check_bit(self.reg_mode(), 24)
     }
 
     fn toggle_reception_frame_packing(&mut self, on: bool) -> bool {
@@ -377,7 +380,7 @@ impl bcm2711_pcm_driver {
             false
         }
         else {
-            self.set_bit(MODE_A, 25, on)
+            self.set_bit(self.reg_mode(), 25, on)
         }
     }
 
@@ -386,12 +389,12 @@ impl bcm2711_pcm_driver {
             false
         }
         else {
-            self.set_bit(MODE_A, 24, on)
+            self.set_bit(self.reg_mode(), 24, on)
         }
     }
 
     fn get_pcm_clock_mode(&mut self) -> u32 {
-        if self.check_bit(MODE_A, 23) { 1 } else { 0 }
+        if self.check_bit(self.reg_mode(), 23) { 1 } else { 0 }
     }
 
     fn set_pcm_clock_mode(&mut self, mode: u32) -> bool {
@@ -399,12 +402,12 @@ impl bcm2711_pcm_driver {
             false 
         }
         else { 
-            self.set_bit(MODE_A, 23, if mode == 0 { false } else { true })
+            self.set_bit(self.reg_mode(), 23, if mode == 0 { false } else { true })
         }
     }
 
     fn get_pcm_clock_inversion(&mut self) -> u32 {
-        if self.check_bit(MODE_A, 22) { 1 } else { 0 }
+        if self.check_bit(self.reg_mode(), 22) { 1 } else { 0 }
     }
 
     fn set_pcm_clock_inversion(&mut self, mode: u32) -> bool {
@@ -412,12 +415,12 @@ impl bcm2711_pcm_driver {
             false 
         }
         else { 
-            self.set_bit(MODE_A, 22, if mode == 0 { false } else { true })
+            self.set_bit(self.reg_mode(), 22, if mode == 0 { false } else { true })
         }
     }
 
     fn get_pcm_frame_sync_mode(&mut self) -> u32 {
-        if self.check_bit(MODE_A, 21) { 1 } else { 0 }
+        if self.check_bit(self.reg_mode(), 21) { 1 } else { 0 }
     }
 
     fn set_pcm_frame_sync_mode(&mut self, mode: u32) -> bool {
@@ -425,12 +428,12 @@ impl bcm2711_pcm_driver {
             false 
         }
         else { 
-            self.set_bit(MODE_A, 21, if mode == 0 { false } else { true })
+            self.set_bit(self.reg_mode(), 21, if mode == 0 { false } else { true })
         }
     }
 
     fn get_pcm_frame_sync_inversion(&mut self) -> u32 {
-        if self.check_bit(MODE_A, 20) { 1 } else { 0 }
+        if self.check_bit(self.reg_mode(), 20) { 1 } else { 0 }
     }
 
     fn set_pcm_frame_sync_inversion(&mut self, mode: u32) -> bool {
@@ -438,12 +441,12 @@ impl bcm2711_pcm_driver {
             false 
         }
         else { 
-            self.set_bit(MODE_A, 20, if mode == 0 { false } else { true })
+            self.set_bit(self.reg_mode(), 20, if mode == 0 { false } else { true })
         }
     }
 
     fn get_pcm_frame_length(&mut self) -> u32 {
-        self.read_bits(MODE_A, 10, 10) + 1
+        self.read_bits(self.reg_mode(), 10, 10) + 1
     }
 
     fn set_pcm_frame_length(&mut self, length: u32) -> bool {
@@ -451,13 +454,13 @@ impl bcm2711_pcm_driver {
             false
         }
         else {
-            self.write_bits(MODE_A, 10, 10, (length - 1) & ((1 << 10) - 1));
+            self.write_bits(self.reg_mode(), 10, 10, (length - 1) & ((1 << 10) - 1));
             true
         }
     }
 
     fn get_pcm_frame_sync_length(&mut self) -> u32 {
-        self.read_bits(MODE_A, 0, 10)
+        self.read_bits(self.reg_mode(), 0, 10)
     }
 
     // only in master frame sync mode
@@ -466,8 +469,28 @@ impl bcm2711_pcm_driver {
             false
         }
         else {
-            self.write_bits(MODE_A, 0, 10, length & ((1 << 10) - 1));
+            self.write_bits(self.reg_mode(), 0, 10, length & ((1 << 10) - 1));
             true
         }
     }
+
+    // acceptable range is 8-39
+    // channel 1 or channel 2
+    // fn get_channel_width(&mut self, kind: u32, num: u32) -> u32 {
+    //     self.read_bits(reg, start, len)
+    // }
+
+    // fn set_channel_width(&mut self, kind: u32, num: u32, width: u32) -> bool {
+    //     if self.pcm_is_enabled() || width < 8 || width > 39 || num < 1 || num > 2 {
+    //         false
+    //     }
+    //     else {
+    //         let true_width = width - 8;
+    //         let reg: usize = RXC_A + kind; // CHANNEL_RXC or CHANNEL_TXC
+    //         let start: usize = (2 - num) * 16;
+    //         self.write_bits(reg, start, 4, true_width & ((1 << 4) - 1));
+    //         self.set_bit_force(reg, start + 15, if (true_width & ((1 << 5) - 1)) >> 4 == 0 { false } else { true });
+    //         true
+    //     }
+    // }
 }
