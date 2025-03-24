8d31f03de8de10e2d934ff0e250ddcaa0b6ec519
juice <hexxated@protonmail.com>
2025-03-24 17:54:32 -0500

    removed .o files oopsies

--- crates/networking/target/debug/deps/byteorder-cfa35b80a9106189.d
+++
@@ -1,6 +0,0 @@
-/home/hexated/academics/pincerOS/networking/networking/target/debug/deps/libbyteorder-cfa35b80a9106189.rmeta: /home/hexated/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs /home/hexated/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/io.rs
-
-/home/hexated/academics/pincerOS/networking/networking/target/debug/deps/byteorder-cfa35b80a9106189.d: /home/hexated/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs /home/hexated/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/io.rs
-
-/home/hexated/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs:
-/home/hexated/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/io.rs:
