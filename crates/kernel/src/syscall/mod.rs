a061c7c3a7437588fe10e6ccaa8484c2cefcffe3
Alex Meyer <ameyer@cs.utexas.edu>
2025-03-24 12:58:13 -0500

    Usermode framebuffer access

--- crates/kernel/src/syscall/mod.rs
+++ crates/kernel/src/syscall/mod.rs
@@ -2,6 +2,7 @@ use crate::event::exceptions::register_syscall_handler;
 
 pub mod channel;
 pub mod exec;
+pub mod fb_hack;
 pub mod file;
 pub mod mmap;
 pub mod pipe;
@@ -30,5 +31,7 @@ pub unsafe fn register_syscalls() {
 
         register_syscall_handler(18, mmap::sys_mmap);
         register_syscall_handler(19, mmap::sys_munmap);
+
+        register_syscall_handler(23, fb_hack::sys_acquire_fb);
     }
 }
