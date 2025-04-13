15ad97c2345e4e3311340561157ef062281900e6
juice <hexxated@protonmail.com>
2025-04-13 18:58:12 -0500

    need to redo sends

---
+++ crates/kernel/src/networking/iface/cdcecm.rs
@@ -0,0 +1,32 @@
+use crate::networking::{Error, Result};
+use crate::networking::repr::dev::Device;
+
+use crate::device::usb::device::net::*;
+
+pub struct CDCECM {
+    max_transmission_unit: usize,
+}
+
+impl Device for CDCECM {
+    // TODO: fn send(&mut self, buffer: &[u8], buffer_len: u32) -> Result<()> {
+    fn send(&mut self, buffer: &mut [u8], buffer_len: u32) {
+        unsafe {
+            NetSendPacket(buffer.as_mut_ptr(), buffer_len);
+        }
+    }
+
+    // fn recv(&mut self, buffer: &mut [u8], buffer_len: u32) -> Result<usize> {
+    fn recv(&mut self, buffer: &mut [u8], buffer_len: u32) {
+        NetReceive(buffer.as_mut_ptr(), buffer_len);
+    }
+
+    fn mtu(&self) -> usize {
+        self.max_transmission_unit
+    }
+}
+
+// impl Drop for CDCECM {
+//     fn drop(&mut self) {
+//
+//     }
+// }
