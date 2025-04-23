1af324651380d33131095135995ab4b495be202d
Alex Meyer <ameyer@cs.utexas.edu>
2025-04-22 19:32:23 -0500

    Add include_bytes_align macro in ulib

--- crates/console/src/main.rs
+++ crates/console/src/main.rs
@@ -15,29 +15,6 @@ mod vt100;
 use display_client::proto;
 use gfx::{color, format};
 
-#[macro_use]
-#[doc(hidden)]
-pub(crate) mod macros {
-    // https://users.rust-lang.org/t/can-i-conveniently-compile-bytes-into-a-rust-program-with-a-specific-alignment/24049/2
-    #[repr(C)]
-    pub struct AlignedAs<Align, Bytes: ?Sized> {
-        pub _align: [Align; 0],
-        pub bytes: Bytes,
-    }
-    macro_rules! include_bytes_align {
-        ($align_ty:ty, $path:literal) => {{
-            // const block expression to encapsulate the static
-            use $crate::macros::AlignedAs;
-            // this assignment is made possible by CoerceUnsized
-            static ALIGNED: &AlignedAs<$align_ty, [u8]> = &AlignedAs {
-                _align: [],
-                bytes: *include_bytes!($path),
-            };
-            &ALIGNED.bytes
-        }};
-    }
-}
-
 use ulib::sys::{pipe, pwrite_all, spawn_elf, FileDesc, SpawnArgs};
 
 #[no_mangle]
