ca2481502e4373fac74e08fd5101fd6ea22e21eb
Alex Meyer <ameyer@cs.utexas.edu>
2025-04-22 19:32:23 -0500

    Image rendering ("show" command) demo

--- crates/kernel/scripts/compile-init.sh
+++ crates/kernel/scripts/compile-init.sh
@@ -8,4 +8,5 @@ FS_PATH="../../disk-image"
 DESTDIR="$FS_PATH" ../shell/build.sh
 DESTDIR="$FS_PATH" ../console/build.sh
 DESTDIR="$FS_PATH" ../display-server/build.sh
+DESTDIR="$FS_PATH" ../show/build.sh
 ../init/build.sh
---
+++ crates/show/build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -ex
+cd "$(dirname "$0")"
+
+BIN="show"
+TARGET=aarch64-unknown-none-softfloat
+PROFILE=${PROFILE-"release"}
+
+cargo rustc --profile="${PROFILE}" \
+    --target="${TARGET}" \
+    --bin="${BIN}" \
+    -- -C relocation-model=static
+
+if test "$PROFILE" = "dev" ; then
+    BINARY=../../target/${TARGET}/debug/${BIN}
+else
+    BINARY=../../target/${TARGET}/${PROFILE}/${BIN}
+fi
+
+cp "${BINARY}" "${BIN}".elf
+
+if test -n "$DESTDIR" ; then
+    cp "${BINARY}" "$DESTDIR/"
+fi
