7823e6a0c5cdfde3b596bc80140478bf692051f7
juice <hexxated@protonmail.com>
2025-04-26 15:03:36 -0500

    revert main example, fix import

--- crates/kernel/examples/main.rs
+++ crates/kernel/examples/main.rs
@@ -5,13 +5,7 @@ extern crate alloc;
 extern crate kernel;
 
 use kernel::event::thread;
-// use kernel::networking::repr::Ipv4Address;
-// use kernel::networking::socket::{
-//     bind, connect, recv_from, send_to, SocketAddr, TcpSocket, UdpSocket,
-// };
 use kernel::*;
-
-// use core::str;
 
 #[no_mangle]
 extern "Rust" fn kernel_main(_device_tree: device_tree::DeviceTree) {
@@ -32,52 +26,4 @@ extern "Rust" fn kernel_main(_device_tree: device_tree::DeviceTree) {
     }
     barrier.sync_blocking();
     println!("End of preemption test");
-
-    // [udp send test]
-    // let s = UdpSocket::new();
-    // let saddr = SocketAddr {
-    //     addr: Ipv4Address::new([11, 187, 10, 102]),
-    //     port: 2222,
-    // };
-    // send_to(s, "hello everynyan".as_bytes().to_vec(), saddr);
-
-    // [udp listening test]
-    // let s = UdpSocket::new();
-    //
-    // bind(s, 2222);
-    //
-    // loop {
-    //     sync::spin_sleep(500_000);
-    //     let recv = recv_from(s);
-    //     if let Ok((payload, senderaddr)) = recv {
-    //         println!("got message: {:x?}", payload);
-    //     }
-    // }
-
-    // [tcp send test]
-    // for i in 0..count {
-    //     sync::spin_sleep(100_000);
-    // }
-    // let saddr = SocketAddr {
-    //     addr: Ipv4Address::new([11, 187, 10, 102]),
-    //     port: 2222,
-    // };
-    //
-    // let s = TcpSocket::new();
-    // connect(s, saddr);
-    // // TODO: connect, recv, and dhcp init really need blocking
-    // for i in 0..count {
-    //     sync::spin_sleep(100_000);
-    // }
-    // send_to(s, "hello everynyan".as_bytes().to_vec(), saddr);
-
-    // [tcp recv test]
-    // let s = TcpSocket::new();
-    //
-    // bind(s, 2222);
-    // listen(s);
-    //
-    // let client = accept(s);
-    //
-    // recv_from(s, client);
 }
