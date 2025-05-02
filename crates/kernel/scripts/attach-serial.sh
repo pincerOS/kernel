f960491f24d2f929b1dc2a6a000df615dab28b05
Aaron Lo <aaronlo0929@gmail.com>
2025-05-01 21:49:55 -0500

    update scripts file

--- crates/kernel/scripts/attach-serial.sh
+++ crates/kernel/scripts/attach-serial.sh
@@ -1,4 +1,4 @@
-SERIAL_DEV=/dev/tty.usbserial-B0044ASI
+SERIAL_DEV=${1-"-l"}
 LOGFILE=log/$(date -Is).log
 echo "Logging to $LOGFILE"
 tio "$SERIAL_DEV" -b 115200 -d 8 -s 1 -p none -f none -m INLCRNL -L --log-file="$LOGFILE"
